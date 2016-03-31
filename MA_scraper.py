0693cfc1f5f05c9ba3ba4c8ab784640f5b77e3b2
jonchar <jonchar@users.noreply.github.com>
2016-03-31 16:48:03 -0400

    Add comments to MA_scraper.py

--- MA_scraper.py
+++ MA_scraper.py
@@ -7,28 +7,39 @@ BASEURL = 'http://www.metal-archives.com'
 RELURL = '/browse/ajax-letter/json/1/l/'
 response_len = 500
 
-def get_url(letter='A', start=0, length=response_len):
-    payload = {'sEcho': 0,
-               'iDisplayStart': start,
-               'iDisplayLength': length}
+def get_url(letter='A', start=0, length=500):
+    """Gets the listings displayed as alphabetical tables on M-A for input `letter`,
+    starting at `start` and ending at `start` + `length`.
+    Returns a `Response` object, containing the data in JSON format."""
+    
+    payload = {'sEcho': 0,               # if not set, text of the response is not valid JSON
+               'iDisplayStart': start,   # set starting index of band names returned
+               'iDisplayLength': length} # only response lengths of 500 appear to work
+    
     r = requests.get(BASEURL + RELURL + letter, params=payload)
+    
     return r
 
+# Data columns returned in the JSON object
 column_names = ['NameLink', 'Country', 'Genre', 'Status']
+data = DataFrame() # for collecting the results
 
+# Valid inputs for the `letter` parameter of the URL are NBR or A through Z
 letters = 'NBR A B C D E F G H I J K L M N O P Q R S T U V W X Y Z'.split()
-
-data = DataFrame()
-
 date_of_parsing = datetime.datetime.utcnow().strftime('%Y-%m-%d')
 
+# Retrieve the data
 for letter in letters:
+    
+    # Get total records for given letter & calculate number of chunks
     print('Current letter = ', letter)
     r = get_url(letter=letter, start=0, length=response_len)
     js = r.json()
     n_records = js['iTotalRecords']
     n_js_files = int(n_records / response_len) + 1
     print('Total records = ', n_records)
+    
+    # Retrieve chunks
     for i in range(n_js_files):
         start = response_len * i
         if start + response_len < n_records:
@@ -36,20 +47,25 @@ for letter in letters:
         else:
             end = n_records
         print('Fetching records ', start, 'to ', end)
+        
         for attempt in range(10):
             try:
                 r = get_url(letter=letter, start=start, length=response_len)
                 js = r.json()
+                # Store response
                 df = DataFrame(js['aaData'])
                 data = data.append(df)
+            # If the response fails the output won't be valid JSON, so retry
             except JSONDecodeError:
                 print('JSONDecodeError on attempt ', attempt, ' of 10.')
                 print('Retrying...')
                 continue
             break
 
+# Set informative names
 data.columns = column_names
 print('Writing data to csv...')
+# Save to CSV
 data.to_csv('MA-band-names_' + date_of_parsing + '.csv')
 print('Complete!')
 
